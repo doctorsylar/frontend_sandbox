4bd4168073e18744099854590ffe0dcdfd206bdb
doctorsylar <soul.evans.15@gmail.com>
2020-06-07 19:37:48 +0300

    altered build

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 console.log(123);
+$(function () {
+    for (let i = 0; i < 10; i++) {
+        init();
+    }
+});
+function init() {
+    $('#test_btn').click(function () {
+        console.log('test');
+    })
+}
